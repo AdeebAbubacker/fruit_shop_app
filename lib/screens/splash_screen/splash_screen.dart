81bb447ed4e60dff81de144272231f30ae9b0643
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-07-11 01:22:05 +0530

    splash screen created

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_shop_app/core/constants/common.dart';
+import 'package:fruit_shop_app/core/constants/const.dart';
+import 'package:fruit_shop_app/core/constants/text_styles.dart';
+import 'package:fruit_shop_app/screens/home_screen/home_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
+          },
+        ),
+      );
+    });
+    double screenheight = screenHeight(context);
+    double height44 = screenheight * 44 / FigmaConstants.figmaDeviceHeight;
+    double height8 = screenheight * 8 / FigmaConstants.figmaDeviceHeight;
+
+    return Scaffold(
+      body: Center(
+        child: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/vegan.png',
+                width: 180,
+              )
+            ],
+          ),
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                Text(
+                  "Version 1.0.14",
+                  style: TextStyles.rubikregular14black3B,
+                ),
+                SizedBox(height: height8),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Copyright @ 2024 Vegan",
+                    style: TextStyles.rubikregular14grey66,
+                  ),
+                ),
+                SizedBox(
+                  height: height44,
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
